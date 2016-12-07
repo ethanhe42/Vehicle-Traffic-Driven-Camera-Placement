973ec636b1657fb1ad6846f0c45e268639eb9e22
yihui-he <yihuihe@foxmail.com>
2016-12-07 15:48:03 +0800

    50

--- roads.py
+++ roads.py
@@ -1,4 +1,5 @@
 import os
+import matplotlib.pyplot as plt
 from utils import *
 from IPython import embed
 from collections import Counter
@@ -7,8 +8,6 @@ from os import listdir
 from os.path import isfile, join
 import pandas as pd
 import numpy as np
-
-
 
 ProcNum = 20
 duration = 24*3600
@@ -24,42 +23,23 @@ lat_min=39
 lat_max=42
 mypath = 'geo.csv'
 rawdata=pd.read_csv(mypath,header=None,names=['id','road','lat','lon'])#,skip_blank_lines=True)
-block = square_decode(rawdata['lon'], rawdata['lat'],square_len=square_len )
+outs=None
+for square_len in [1000,500,100,80,50,40,30,20,10]:
+    block = square_decode(rawdata['lon'], rawdata['lat'],square_len=square_len )
+    block.name=str(square_len)
+    newdf = rawdata.join(block)
+    newdf = newdf.dropna()
+    roadgbblock = newdf['road'].groupby(newdf[block.name])
+    tmp = roadgbblock.nunique().value_counts()
+    tmp.name=block.name
+    if outs is None:
+        outs = pd.DataFrame(tmp)
+    else:
+        outs = outs.join(tmp)
+outs.to_csv()
+cu = 5
+newouts = outs[outs.index<cu]
+newouts.loc[cu]=outs[outs.index>=cu].sum()
 embed()
-
-
-
-
-block
-pd.unique?
-rawdata.join?
-rawdata.join(block)
-block.name='block'
-newdf = rawdata.join(block)
-newdf
-newdf.groupby(block)
-newdf.groupby(block).unique()
-newdf['id'].groupby('block')
-newdf['id'].groupby(block)
-newdf['road'].groupby(block)
-roadgbblock = newdf['road'].groupby(block)
-roadgbblock.nunique?
-roadgbblock.nunique??
-roadgbblock.unique?
-roadgbblock.nunique()
-pd.unique?
-pd.unique([1,1,2])
-pd.nunique([1,1,2])
-a=pd.Series([1,1,2])
-a.nunique()
-newdf['block']==32502656
-newdf[newdf['block']==32502656]
-newdf = newdf.dropna()
-roadgbblock = newdf['road'].groupby(newdf['block'])
-roadgbblock.nunique()
-roadgbblock.nunique().hist()
+newouts.plot.bar()
 plt.savefig('50.png')
-import matplotlib.pyplot as plt
-plt.savefig('50.png')
-%save -f tmp.py
-%history -f tmp.py
